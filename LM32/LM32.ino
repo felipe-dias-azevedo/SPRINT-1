4c206d174a53435cf14f323c8da79a7b87cae635
ryan-silva <61800303+ryan-silva@users.noreply.github.com>
2020-03-10 11:24:23 -0300

    Update LM32.ino

--- LM32/LM32.ino
+++ LM32/LM32.ino
@@ -12,7 +12,7 @@ void loop() {
   valorLido = analogRead (5);
   temperatura = (valorLido * 0.00488);
   temperatura = temperatura * 100;
-  Serial.print(teperatura);
+  Serial.print(temperatura);
   Serial.println("");
   delay(1000);
  
